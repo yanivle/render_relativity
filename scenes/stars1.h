9423fc3ba951875084638111b63d088feacfe27f
Yaniv Leviathan <leviathan@leviathan-macbookpro3.roam.corp.google.com>
2019-07-23 00:15:21 -0400

    .

--- scenes/stars1.h
+++ scenes/stars1.h
@@ -24,38 +24,41 @@ void AddBigStar(const Color& color1, const Color& color2, float perlin_scale,
     star = scene->own(new PerlinDeformation((Sphere*)star, deformation_scale, deformation_magnitude));
   }
   star = scene->addObject(new Translate(star, center));
-  scene->addMass((PointMass(center, mass)));
+  if (mass > 0) {
+    scene->addMass((PointMass(center, mass)));
+  }
 }
 
 void createStars1Scene(Scene *res) {
   // Sun.
-  AddBigStar(Color(255, 255, 0), Color(255, 255, 255), 1, 0.0, 1.0, 2, 20, 20+100, vec3(-100, 0, 0), 0, res);
+  AddBigStar(Color(255, 255, 0), Color(255, 255, 255), 1, 0.0, 1.0, 2, 20, 20+25, vec3(-25, 0, 0), 0, res);
 
-  /*
   // Mercuri.
-  AddBigStar(Color(161, 0, 0), Color(100, 50, 0), 15, 0.0, 0.7, 0.1, 0.1, 0.2, vec3(20 + 2, 0, 0), 0, res);
+  AddBigStar(Color(161, 0, 0), Color(100, 50, 0), 15, 0.0, 0.1, 0.1, 0.1, 0.2, vec3(20 + 2, 0, 0), 0, res);
 
   // Venus.
-  AddBigStar(Color(161, 0, 0), Color(100, 50, 0), 15, 0.0, 0.7, 0.1, 0.1, 0.4, vec3(20 + 4, 0, 0), 0, res);
+  AddBigStar(Color(161, 0, 0), Color(100, 50, 0), 15, 0.0, 0.1, 0.1, 0.1, 0.4, vec3(20 + 4, 0, 0), 0, res);
 
   // Earth.
-  AddBigStar(Color(79, 76, 176), Color(106, 147, 214), 15, 0.0, 0.7, 0.1, 0.1, 0.4, vec3(20 + 6, 0, 0), 0, res);
+  AddBigStar(Color(79, 76, 176), Color(106, 147, 214), 15, 0.0, 0.1, 0.1, 0.1, 0.4, vec3(20 + 6, 0, 0), 0, res);
 
   // Mars.
-  AddBigStar(Color(161, 0, 0), Color(100, 50, 0), 15, 0.0, 0.7, 0.1, 0.1, 0.1, vec3(20 + 8, 0, 0), 0, res);
+  AddBigStar(Color(161, 0, 0), Color(100, 50, 0), 15, 0.0, 0.1, 0.1, 0.1, 0.1, vec3(20 + 8, 0, 0), 0, res);
 
   // Jupiter.
-  AddBigStar(Color(0xF6, 0xF7, 0xD2), Color(0xF9, 0xF4, 0x8F), 15, 0.0, 0.7, 0.1, 0.1, 1, vec3(20 + 10, 0, 0), 0, res);
+  AddBigStar(Color(0xF6, 0xF7, 0xD2), Color(0xF9, 0xF4, 0x8F), 15, 0.0, 0.1, 0.1, 0.1, 1, vec3(20 + 10, 0, 0), 0, res);
 
   // Saturn.
-  AddBigStar(Color(161, 0, 0), Color(100, 50, 0), 15, 0.0, 0.7, 0.1, 0.1, 0.8, vec3(20 + 14, 0, 0), 0, res);
+  AddBigStar(Color(161, 0, 0), Color(100, 50, 0), 15, 0.0, 0.1, 0.1, 0.1, 0.8, vec3(20 + 14, 0, 0), 0, res);
 
   // Uranus.
-  AddBigStar(Color(79, 76, 176), Color(106, 147, 214), 15, 0.0, 0.7, 0.1, 0.1, 0.5, vec3(20 + 18, 0, 0), 0, res);
+  AddBigStar(Color(79, 76, 176), Color(106, 147, 214), 15, 0.0, 0.1, 0.1, 0.1, 0.5, vec3(20 + 18, 0, 0), 0, res);
 
   // Neptune.
-  AddBigStar(Color(79, 76, 176), Color(106, 147, 214), 15, 0.0, 0.7, 0.1, 0.1, 0.5, vec3(20 + 20, 0, 0), 0, res);
-  */
+  AddBigStar(Color(79, 76, 176), Color(106, 147, 214), 15, 0.0, 0.1, 0.1, 0.1, 0.5, vec3(20 + 20, 0, 0), 0, res);
+
+  // Black hole.
+  AddBigStar(Color(0, 0, 0), Color(0, 0, 0), 0, 0.0, 0.0, 0, 0, 0.1, vec3(20 + 8, 0, 0), 20, res);
 
   // const int NUM_BACKGROUND_STARS = 1000;
   // for (int i = 0; i < NUM_BACKGROUND_STARS; ++i) {
@@ -75,8 +78,7 @@ void createStars1Scene(Scene *res) {
 
   // for (int i = 0; i < 50; ++i) {
   for (int i = 0; i < 500; ++i) {
-    // res->addLight(new PointLight(vec3(-100, 0, 0) + vec3::random().normalize() * 121));
-    res->addLight(new PointLight(vec3(-100, 0, 0) + vec3::random().normalize() * 200));
+    res->addLight(new PointLight(vec3(-25, 0, 0) + vec3::random().normalize() * 47));
   }
   // res->addLight(new PointLight(vec3(100, 0, 0)));
   // res->addLight(new PointLight(vec3(100, 10, 0)));
@@ -85,6 +87,8 @@ void createStars1Scene(Scene *res) {
 
   res->modifiable_rendering_params().camera_settings.eye_pos = vec3(26, 0, -80);
   res->modifiable_rendering_params().camera_settings.target = vec3(26, 0, 0);
+
+  res->modifiable_rendering_params().use_gravity = true;
 }
 
 #endif
