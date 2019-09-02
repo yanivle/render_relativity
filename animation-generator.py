6d79d1e4db19778a51f5de70ac8724e6aa0cac75
Yaniv Leviathan <leviathan@leviathan-macbookpro3.roam.corp.google.com>
2019-09-02 00:03:58 -0400

    added animation generator

---
+++ animation-generator.py
@@ -0,0 +1,7 @@
+import imageio
+import glob, os
+
+with imageio.get_writer('output/movie.mp4', mode='I') as writer:
+    for filename in glob.glob("output/output*.ppm"):
+        image = imageio.imread(filename)
+        writer.append_data(image)
