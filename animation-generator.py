9bccde62a1788798da3ae0afabdfcfbb7cdc3fc5
Yaniv Leviathan <leviathan@leviathan-macbookpro3.roam.corp.google.com>
2019-09-02 20:47:07 -0400

    .

--- animation-generator.py
+++ animation-generator.py
@@ -1,7 +1,11 @@
 import imageio
 import glob, os
 
+def getnum(f):
+    return int(''.join(x for x in f if str.isdigit(x)))
+
 with imageio.get_writer('output/movie.mp4', mode='I') as writer:
-    for filename in sorted(glob.glob("output/output*.ppm")):
+    for filename in sorted(glob.glob("output/output*.ppm"), key=getnum):
+        print(filename)
         image = imageio.imread(filename)
         writer.append_data(image)
