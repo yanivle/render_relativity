910f320e49d8166994601bf73040938a4be5a2d0
Yaniv Leviathan <leviathan@leviathan-macbookpro3.roam.corp.google.com>
2019-10-08 11:53:32 -0700

    .

--- animation-generator.py
+++ animation-generator.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python3
+
 import imageio
 import glob, os
+import sys
+import progressbar
 
 def getnum(f):
     return int(''.join(x for x in f if str.isdigit(x)))
 
 with imageio.get_writer('output/movie.mp4', mode='I', fps=24) as writer:
-    for filename in sorted(glob.glob("output/output*.ppm"), key=getnum):
-        print(filename)
+    for filename in progressbar.progressbar(sorted(glob.glob("output/output*.ppm"), key=getnum)):
         image = imageio.imread(filename)
         writer.append_data(image)
